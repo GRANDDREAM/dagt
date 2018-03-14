27a5484909e30206b41bd59ed7fef04a52f43995
DAGTFLY <36876554+DAGTFLY@users.noreply.github.com>
2018-03-14 10:06:59 +0800

    分拆setLockMonth函数适用Redmix编译
    分拆setLockMonth函数适用Redmix编译

--- contracts/DagtPri.sol
+++ contracts/DagtPri.sol
@@ -46,26 +46,26 @@ function setMintNum(uint256 _amount) returns (bool) {
 }
 
 
-function setLockMonth(uint256 _amount)  {
+function setLockMonth()  {
   //2018/5/10 0:0:0 1525881600;2018/6/1 0:0:0 1527782400;2018/7/1 0:0:0 1530374400
   //2018/8/1 0:0:0 1533052800;2018/9/1 0:0:0 1535731200;2018/9/30 23:59:59 1538323199
 
-  if(now< 1525881600 && LockinMonth0==false)
+  if((now< 1525881600) && (LockinMonth0==false))
   {
 
     LockinMonth1=false;
     LockinMonth2=false;
     LockinMonth3=false;
-    LockinMonth4=false;d
+    LockinMonth4=false;
     LockinMonth0 = true;
     mintedNums=0;
   }
-  if(now< 1525881600 && LockinMonth0==true && mintedNums>LOCK_NUMS_SUPPLY)
+  if((now< 1525881600) && (LockinMonth0==true) && (mintedNums>LOCK_NUMS_SUPPLY))
   {
     LockinMonth0 = false;
   }
    //e
-    if( now >=1525881600 && now< 1527782400 && LockinMonth1==false)
+    if( (now >=1525881600) && (now< 1527782400) && (LockinMonth1==false))
     {
       LockinMonth0 = false;
       LockinMonth2=false;
@@ -74,12 +74,12 @@ function setLockMonth(uint256 _amount)  {
       LockinMonth1 = true;
       mintedNums=0;
     }
-    if( now >=1525881600 && now< 1527782400 && LockinMonth1==true && mintedNums>LOCK_NUMS_SUPPLY)
+    if( (now >=1525881600) && (now< 1527782400) && (LockinMonth1==true) && (mintedNums>LOCK_NUMS_SUPPLY))
     {
       LockinMonth1 = false;
     }
     //
-    if( now >=1527782400 && now< 1530374400 && LockinMonth2==false)
+    if( (now >=1527782400) && (now< 1530374400) && (LockinMonth2==false))
     {
       LockinMonth0 = false;
       LockinMonth1=false;
@@ -88,12 +88,19 @@ function setLockMonth(uint256 _amount)  {
       LockinMonth2 = true;
       mintedNums=0;
     }
-    if( now >=1527782400 && now< 1530374400 && LockinMonth2==true && mintedNums>LOCK_NUMS_SUPPLY)
+    if((now >=1527782400) && (now< 1530374400) && (LockinMonth2==true) && (mintedNums>LOCK_NUMS_SUPPLY))
     {
       LockinMonth2 = false;
     }
-    //
-    if( now >=1530374400 && now< 1533052800 && LockinMonth3==false)
+
+
+  //  return (now,LockinMonth0,LockinMonth1,LockinMonth2,LockinMonth3,LockinMonth4);
+
+}
+
+function setLockMonth_2()
+{
+     if( (now >=1530374400) && (now< 1533052800) && (LockinMonth3==false))
     {
       LockinMonth0 = false;
       LockinMonth1=false;
@@ -102,7 +109,7 @@ function setLockMonth(uint256 _amount)  {
       LockinMonth3 = true;
       mintedNums=0;
     }
-    if( now >=1530374400 && now< 1533052800 && LockinMonth3==true && mintedNums>LOCK_NUMS_SUPPLY)
+    if( (now >=1530374400) && (now< 1533052800) && (LockinMonth3==true) && (mintedNums>LOCK_NUMS_SUPPLY))
     {
       LockinMonth3 = false;
     }
@@ -122,6 +129,11 @@ function setLockMonth(uint256 _amount)  {
       LockinMonth4 = false;
     }
 
+
+}
+
+function setMouthEnable(uint256 _amount)
+{
     if((mintedNums+_amount)>LOCK_NUMS_SUPPLY)
     {
       LockinMonth0 = false;
@@ -130,15 +142,14 @@ function setLockMonth(uint256 _amount)  {
       LockinMonth3=false;
       LockinMonth4=false;
     }
-  //  return (now,LockinMonth0,LockinMonth1,LockinMonth2,LockinMonth3,LockinMonth4);
-
 }
-
 
 
 function mint(address _to, uint256 _amount)  public returns (bool)
 {
-     setLockMonth(_amount);
+     setLockMonth();
+     setLockMonth_2();
+     setMouthEnable(_amount);
      if(LockinMonth0==true||LockinMonth1==true||LockinMonth2==true||LockinMonth3==true||LockinMonth4==true)
      {
        bool ret =  super.mint(_to,_amount);
