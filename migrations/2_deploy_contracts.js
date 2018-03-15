270082d84f9e8d286f129661ff5b0a9c6c227b70
dingzhongchang <zhongchang.ding@gmail.com>
2018-03-15 19:49:16 +0800

    deploy

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,8 @@
 var DagtPri = artifacts.require("DagtPri.sol");
-var DagtPub = artifacts.require("DagtPub.sol");
+var Dagt = artifacts.require("Dagt.sol");
 module.exports = function(deployer) {
+  deployer.deploy(Dagt);
+  deployer.link(Dagt,DagtPri);
   deployer.deploy(DagtPri);
-  deployer.deploy(DagtPub);
+  
 };
